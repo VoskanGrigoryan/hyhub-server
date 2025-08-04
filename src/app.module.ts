27af997e3cffc4168689dd775103afc47fd40281
VoskanGrigoryan <voskan.grigoryan.arg@gmail.com>
2025-08-04 02:52:26 -0300

    adding strategy and google credentials to .env.prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { JwtStrategy } from './modules/auth/strategy/auth.strategy';
+import { GoogleStrategy } from './modules/auth/strategy/auth.google.strategy';
 
 @Module({
   imports: [PrismaModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [GoogleStrategy, JwtStrategy],
 })
 export class AppModule {}
